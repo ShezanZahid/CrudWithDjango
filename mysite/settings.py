6759c26a4200b18dffd57641bac5a8050f38017d
ShezanZahid <63417451+ShezanZahid@users.noreply.github.com>
2021-08-31 14:11:20 +0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -153,7 +153,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'shezan.new@gmail.com'
-EMAIL_HOST_PASSWORD = '9141496SS'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 #User Session
